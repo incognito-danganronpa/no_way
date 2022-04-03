c84ccb45660cc0bfe7ef47f2e366ac786f5dd231
incognito-danganronpa <100456768+incognito-danganronpa@users.noreply.github.com>
2022-04-03 09:53:28 +0800

    Initial commit

---
+++ src/Demo.java
@@ -0,0 +1,34 @@
+// Written by **insert name here**
+// A demonstration of basic Git commands & practice of declaring variables in Java
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		
+		// declare 4 variables
+		// TODO: Switch out the values for each variable to match your personal preferences
+		
+		int myFavoriteInteger = 19;
+		double myLeastFavoriteDouble = 0.333;
+		boolean camelCase = true;
+		char firstInitial = 'S';
+		
+		
+		// TODO: Declare 4 new variables, an integer, double, boolean, and character
+		// Make sure to name using camelCasing format
+		
+		
+		
+		// print out each starter variable
+		System.out.println("My Started Variables:");
+		System.out.println(myFavoriteInteger);
+		System.out.println(myLeastFavoriteDouble);
+		System.out.println(camelCase);
+		System.out.println(firstInitial);
+		
+		
+		// TODO: Add 4 print statements for the variables you declared
+		System.out.println("\nYour Variables:");
+		
+	}
+}
